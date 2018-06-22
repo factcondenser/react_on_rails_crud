8b97db78179ea93c077ae4d222a1dbedf6a095ef
factcondenser <markpan@live.com>
2018-06-22 02:18:53 -0500

    Add react-rails and an index view for the root path

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,15 @@
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    );
+  }
+}
