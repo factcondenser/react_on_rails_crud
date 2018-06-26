8978eabbd94486a932efca07d1a3fef49b1b6509
factcondenser <markpan@live.com>
2018-06-25 23:07:28 -0500

    Add new item form

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -2,19 +2,55 @@ class NewItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      name: '',
+      description: ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentWillMount() {
-    console.log('NewItem component about to mount.');
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const fieldName = target.name;
+
+    this.setState({
+      [fieldName]: value.toUpperCase()
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.name + ': ' + this.state.description)
+    event.preventDefault();
   }
 
   render() {
     return(
-      <div>
-        <h2>NewItem component goes here.</h2>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            name='name'
+            type='text'
+            placeholder='Enter the name of the item.'
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Description:
+          <input
+            name='description'
+            type='text'
+            placeholder='Enter the name of the description'
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
